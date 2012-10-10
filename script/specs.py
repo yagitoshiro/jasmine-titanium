99fcc82e34b1746f678adadb06fc5f24ba401eb3
Toshiro Yagi <yagitoshiro@gmail.com>
2012-10-11 04:06:52 +0900

    update sdk_version

--- script/specs.py
+++ script/specs.py
@@ -3,6 +3,7 @@
 
 import os, sys, re, shutil, time, subprocess
 from optparse import OptionParser
+from xml.dom import minidom
 
 def project_dir():
     return resource_dir().replace("/Resources", "")
@@ -14,13 +15,10 @@ def resource_dir():
     return re.sub("%(sep)sResources%(sep)s.+" % locals(), "%(sep)sResources" % locals(), script_dir)
 
 def sdk_version():
-    setting_path = os.path.join(project_dir(), ".settings", "com.appcelerator.titanium.mobile.prefs")
-    version = ''
-    version_constant_name = "MOBILE_PROJECT_SDK_VERSION"
-    for line in open(setting_path, 'r'):
-        if line.startswith(version_constant_name):
-            version = re.sub(version_constant_name + "=", "", line.strip())
-        
+    tiapp_path = os.path.join(project_dir(), "tiapp.xml")
+    tiapp_file = minidom.parse(tiapp_path)
+    version_dom = tiapp_file.getElementsByTagName("sdk-version")[0]
+    version = version_dom.childNodes[0].data
     return version
 
 def options_temporary_path():
